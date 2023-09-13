83d4918eeb9b0d57eb2cab6e52784a71c96ec2dd
Tas33n <tasu.legend@gmail.com>
2023-09-13 12:42:28 +0600

    intial release

---
+++ README.md
@@ -0,0 +1,75 @@
+<p align="center">
+  <a href="https://github.com/tas33n/AsuraScans-DL-Bot">
+    <img src="[images/image1.png](https://asuracomics.com/wp-content/uploads/2023/07/cropped-cropped-Group_1-1-270x270.png)" alt="Logo" width="85" height="85">
+  </a>
+
+  <h3 align="center">AsuraScans (Asuracomics) Manhwa– Downloader</h3>
+
+  <p align="center">
+    <samp>A simple bot to download Manhwa Chapters as PDF from <a href="https://https://asuracomics.com/">AsuraScans/Asuracomics</a></samp>
+    <br />
+    <br />
+    <a href="https://github.com/tas33n/AsuraScans-DL-Bot/issues/new?assignees=riimuru&labels=bug&template=bug-report.yml">Bug report</a>
+    ·
+    <a href="https://github.com/tas33n/AsuraScans-DL-Bot/issues/new?assignees=riimuru&labels=enhancement&template=feature-request.md">Feature request</a>
+  </p>
+  <p align="center">
+     <a href="https://github.com/tas33n/AsuraScans-DL-Bot/actions/workflows/codeql-analysis.yml">
+      <img src="https://github.com/tas33n/AsuraScans-DL-Bot/actions/workflows/codeql-analysis.yml/badge.svg" alt="stars">
+    </a>
+    <a href="https://github.com/tas33n/AsuraScans-DL-Bot">
+      <img src="https://img.shields.io/github/stars/tas33n/AsuraScans-DL-Bot" alt="stars">
+    </a>
+        <a href="https://github.com/consumet/extensions/blob/master/LICENSE">
+      <img src="https://img.shields.io/github/license/consumet/extensions" alt="GitHub">
+    </a>
+  </p>
+</p>
+
+## Installation
+
+### Local
+Run the following command to clone the repository, and install the dependencies:
+
+```sh
+git clone https://github.com/tas33n/AsuraScans-DL-Bot.git
+cd AsuraScans-DL-Bot
+npm install #or yarn install
+```
+
+start the server with the following command:
+
+```sh
+npm start #or yarn start
+```
+Now the server is running on http://localhost:3000
+
+### Railway
+Host your own instance of the api on railway using the button below.
+
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template/Lg6DEp?referralCode=dv4TuD)
+
+## Contributing
+1. [Fork the repository](https://github.com/tas33n/AsuraScans-DL-Bot)
+2. Clone your fork to your local machine using the following command **(make sure to change `<your_username>` to your GitHub username)**:
+```sh
+git clone https://github.com/<your-username>/AsuraScans-DL-Bot.git
+```
+3. Create a new branch: `git checkout -b <new-branch-name>` (e.g. `git checkout -b my-new-branch`)
+4. Make your changes.
+5. Stage the changes: `git add .`
+6. Commit the changes: `git commit -m "My commit message"`
+7. Push the changes to GitHub: `git push origin <new-branch-name>` (e.g. `git push origin my-new-branch`)
+8. Open a pull request.
+
+### Currently supported sites
+<details>
+<summary>Manga/Manhwa</summary>
+
+- [Asuracomics](https://asuracomics.com/)
+</details>
+
+> ### Note:
+> **Your feedback and suggestions are very welcome. Please [open an issue](https://github.com/tas33n/AsuraScans-DL-Bot/issues/new/choose).**
+> This project will still be maintained.
+
---
+++ index.js
@@ -0,0 +1,183 @@
+const { Telegraf } = require('telegraf');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const { PDFDocument, rgb } = require('pdf-lib');
+const sharp = require('sharp');
+const path = require('path');
+
+const bot = new Telegraf('6043015816:AAEWuCk_omHvrFKvS6o1kaBDKQVU4OB1lYs');
+const currentTime = new Date().toLocaleTimeString();
+console.log(`Bot started at ${currentTime}`);
+
+bot.start((ctx) => {
+  ctx.reply('Welcome to the Image to PDF bot! Please send me the URL of the images you want to convert to PDF.');
+});
+
+bot.help((ctx) => {
+  ctx.reply(
+    'Welcome to the Image to PDF bot!\n\n' +
+    'To use this bot, simply send a valid URL that contains images you want to convert to PDF. ' +
+    'The bot will scrape the images and send you a PDF document.\n\n' +
+    'Commands:\n' +
+    '/help - Show this help message\n'
+  );
+});
+
+
+bot.on('text', async (ctx) => {
+  const url = ctx.message.text;
+
+  // Check if the message contains a valid URL
+  // Define a regular expression pattern for the expected URL format
+  const validURLPattern = /^https:\/\/asuracomics\.com\/(\d+-.+?)\/$/;
+
+  // Check if the URL matches the pattern
+  const match = url.match(validURLPattern);
+
+  if (!match) {
+    ctx.reply('Invalid URL. Please provide a valid asuracomics URL.');
+    return;
+  }
+
+  try {
+    // Scrape images and create PDF
+    const folderName = await scrapeImages(url);
+    const pdfPath = await createPdfFromImages(folderName);
+
+    console.log(pdfPath);
+
+    // Send the generated PDF to the user
+    const pdfFileName = path.basename(pdfPath);
+    ctx.replyWithDocument({ source: pdfPath }, { filename: pdfFileName })
+      .then(() => {
+        // File sent successfully, now clean up
+        cleanup(folderName, pdfPath);
+      })
+      .catch((error) => {
+        console.error('Error sending file:', error);
+        // Handle the error here, such as retrying or reporting the issue
+      });
+
+
+  } catch (error) {
+    console.error('Error:', error);
+    ctx.reply('An error occurred while processing the URL.');
+  }
+});
+
+bot.launch();
+
+
+async function scrapeImages(url) {
+  try {
+    const folderName = "tmp/" + url.split('/').filter(Boolean).pop().replace(/^(\d+-)/, '');
+    // Make a GET request to the URL
+    const response = await axios.get(url);
+
+    // Load the HTML content into Cheerio
+    const $ = cheerio.load(response.data);
+
+    // Find the <div id="readerarea"> element
+    const readerArea = $('#readerarea');
+
+    // Find all <img> elements within the <div id="readerarea">
+    const imgElements = readerArea.find('img[decoding="async"][src]');
+
+    // Create a directory for the images
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName);
+    }
+
+    // Create an array to store the image source URLs
+    const imgSrcArray = [];
+
+    // Loop through the img elements and collect the image source URLs
+    imgElements.each((index, element) => {
+      const imgSrc = $(element).attr('src');
+      imgSrcArray.push(imgSrc);
+    });
+
+    console.log(imgSrcArray);
+
+    // Loop through the array of image source URLs and download the images
+    for (let i = 0; i < imgSrcArray.length; i++) {
+      const imgSrc = imgSrcArray[i];
+      const imgName = path.basename(imgSrc);
+      const imgPath = path.join(folderName, imgName);
+
+      // Download the image
+      await axios({
+        method: 'get',
+        url: imgSrc,
+        responseType: 'stream',
+      }).then((response) => {
+        response.data.pipe(fs.createWriteStream(imgPath));
+        console.log(`Downloaded: ${imgPath}`);
+      }).catch((error) => {
+        console.error(`Error downloading image: ${imgSrc}`);
+      });
+    }
+
+    return folderName;
+
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function createPdfFromImages(folderName) {
+
+  const pdfPath = folderName + '.pdf';
+  const imageFiles = fs.readdirSync(folderName);
+  const pdfDoc = await PDFDocument.create();
+  const pdfPages = [];
+
+  for (const imageFile of imageFiles) {
+    const imagePath = path.join(folderName, imageFile);
+
+    // Get the image dimensions dynamically
+    const { width: imageWidth, height: imageHeight } = await sharp(imagePath).metadata();
+
+    const pdfPage = pdfDoc.addPage([imageWidth, imageHeight]);
+    const image = await sharp(imagePath).toBuffer();
+    const imageXObject = await pdfDoc.embedJpg(image);
+
+    pdfPage.drawImage(imageXObject, {
+      x: 0,
+      y: 0,
+      width: imageWidth,
+      height: imageHeight,
+    });
+
+    pdfPages.push(pdfPage);
+  }
+
+  for (const pdfPage of pdfPages) {
+    pdfPage.setFontSize(12);
+    pdfPage.drawText('tg@misfitsdev', {
+      x: 30,
+      y: 30,
+      color: rgb(0, 0, 0),
+    });
+  }
+
+  const pdfBytes = await pdfDoc.save();
+  fs.writeFileSync(pdfPath, pdfBytes);
+
+  return pdfPath;
+}
+
+async function cleanup(folderName, pdfPath) {
+  try {
+    // Remove the temporary folder and its contents
+    fs.rmSync(folderName, { recursive: true });
+
+    // Remove the generated PDF file
+    fs.unlinkSync(pdfPath);
+
+    console.log('Cleanup completed successfully.');
+  } catch (error) {
+    console.error('Cleanup error:', error);
+  }
+}
